f80f2b99a979406f91a2cffa2deed3fcd055288e
invokerx <maxu0410@163.com>
2020-09-10 14:21:12 +0800

    cut the picture

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -9,13 +9,15 @@
 namespace App\Handlers;
 
 use Illuminate\Support\Str;
+use Image;
+
 class ImageUploadHandler
 {
     // 设置允许上传的类型
     protected $allowed_ext = ['jpg', 'png', 'gif', 'jpeg'];
 
     // 保存图片功能
-    public function save($file, $folder, $file_prefix)
+    public function save($file, $folder, $file_prefix, $max_width = false)
     {
 
         $folder_name = "uploads/images/$folder/" . date("Ym/d", time());
@@ -33,11 +35,36 @@ class ImageUploadHandler
 
         // 将图片移动到我们的目标存储路径中
         $file->move($upload_path, $filename);
+        // 如果限制了图片宽度，就进行裁剪
+        if ($max_width && $extension != 'gif') {
+
+            // 此类中封装的函数，用于裁剪图片
+            $this->reduceSize($upload_path . '/' . $filename, $max_width);
+        }
 
         return [
             'path' => config('app.url') . "/$folder_name/$filename"
         ];
 
+    }
+
+    public function reduceSize($file_path, $max_width)
+    {
+        $image = Image::make($file_path);
+
+        // 进行大小调整的操作
+        $image->resize($max_width, null, function ($constraint) {
+
+            // 设定宽度是 $max_width，高度等比例缩放
+            $constraint->aspectRatio();
+
+            // 防止裁图时图片尺寸变大
+            $constraint->upsize();
+
+        });
+
+        // 对图片修改后进行保存
+        $image->save();
     }
 
 }
