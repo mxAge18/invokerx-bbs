e45db57eb70e0252b8a35918e2d0179ef6ed8792
invokerx <maxu0410@163.com>
2020-10-12 16:30:33 +0800

    add purifier dev

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -16,11 +16,14 @@ class TopicObserver
 
     public function updating(Topic $topic)
     {
+
         //
     }
 
     public function saving(Topic $topic)
     {
+
+        $topic->body = clean($topic->body, 'user_topic_body');
         $topic->excerpt = make_excerpt($topic->body);
     }
 }
