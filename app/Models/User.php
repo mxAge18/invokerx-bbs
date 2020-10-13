7defa71db102342b4c067a9517e890ffb4c4029e
invokerx <maxu0410@163.com>
2020-10-13 23:13:15 +0800

    add the notification of the reply's index

--- app/Models/User.php
+++ app/Models/User.php
@@ -78,4 +78,11 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $query->orderBy('id', 'desc');
     }
+
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
+    }
 }
