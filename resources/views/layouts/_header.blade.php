58ce594612e59d14e1e33bc761b761a539c14786
invokerx <maxu0410@163.com>
2020-11-01 12:04:40 +0000

    user admin

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -41,6 +41,13 @@
                             {{ Auth::user()->name }}
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            @can('manage_contents')
+                                <a class="dropdown-item" href="{{url(config('administrator.uri'))}}">
+                                    <i class="fas fa-tachometer-alt mr-2">
+                                        管理后台
+                                    </i>
+                                </a>
+                            @endcan
                             <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">
                                 <i class="far fa-user mr-2"></i>
                                 个人中心
