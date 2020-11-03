f475fa3c83f8b02b9351f8ba35399b5b0e47b996
invokerx <maxu0410@163.com>
2020-11-03 10:18:02 +0000

    user sign up

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -34,5 +35,6 @@ class AppServiceProvider extends ServiceProvider
             return \Auth::user()->hasRole('Founder');
         });
         //
+        JsonResource::withoutWrapping();
     }
 }
