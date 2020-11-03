6ce34481d0222e4792b8b8811ee4f40fc834acf2
invokerx <maxu0410@163.com>
2020-11-03 10:15:39 +0000

    send sms

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -13,7 +13,7 @@ class VerificationCodesController extends Controller
     {
 
 
-        $phone = $request->phone;
+        $phone = "+44" . $request->phone;
         // 生成4位随机数，左侧补0
         $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
         try {
