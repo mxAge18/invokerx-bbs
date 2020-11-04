a70eabf5cc8dda07bb988206f53b8e944e6b859f
invokerx <maxu0410@163.com>
2020-11-04 12:46:18 +0000

    category of topic

---
+++ app/Http/Controllers/Api/CategoriesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoriesController extends Controller
+{
+    //
+
+    public function index()
+    {
+        CategoryResource::wrap('data');
+        return CategoryResource::collection(Category::all());
+    }
+}
